5fcdc0f938e5c17e8b573e19d97e4c603e60c0a4
Roland Foucher <roland.foucher.pro@gmail.com>
2022-01-19 16:52:56 +0100

    init project

---
+++ src/main/java/co/simplon/p16/springboard/SpringboardApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.p16.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/simplon/p16/springboard/SpringboardApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.p16.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
