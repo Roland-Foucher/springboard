b64b741cfdff13a2e66dfc664a73f22d7f9f364a
Roland Foucher <roland.foucher.pro@gmail.com>
2022-02-04 13:18:32 +0100

    request mapping on accountcontroller

--- src/main/java/co/simplon/p16/springboard/Controller/AccountController.java
+++ src/main/java/co/simplon/p16/springboard/Controller/AccountController.java
@@ -5,11 +5,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.simplon.p16.springboard.entity.User;
 import co.simplon.p16.springboard.repository.ArtistRepository;
 import co.simplon.p16.springboard.services.ArtistService;
 
+@RequestMapping("myAccount")
 @Controller
 public class AccountController {
 
@@ -21,7 +23,7 @@ public class AccountController {
 
     
     
-    @GetMapping("/myAccount")
+    @GetMapping("")
     public String showMyAccountPage(Model model, @AuthenticationPrincipal User user){
         System.out.println(user.getRole());
         if(user.getRole().equals("ROLE_ARTIST")){
@@ -32,13 +34,13 @@ public class AccountController {
         return "account/myAccount";
     }
 
-    @GetMapping("/myAccount/profil")
+    @GetMapping("/profil")
     public String showProfil(Model model, @AuthenticationPrincipal User user){
         model.addAttribute("user", user);
         return "account/profil";
     }
 
-    @GetMapping("/myAccount/modify")
+    @GetMapping("/modify")
     public String modifyProfil(Model model, @AuthenticationPrincipal User user){
         model.addAttribute("user", user);
         return "login/register";
